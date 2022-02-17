971edc1db51d87c388f369d72bf6f6f060875c53
Codeguer <15946817309@163.com>
2022-02-17 19:15:46 +0800

    2022/2/17

---
+++ C++回顾50/Test.cpp
@@ -0,0 +1,15 @@
+#include"string.h"
+
+//模拟实现string类
+
+int main()
+{
+	//bit::test_string1();
+	//bit::test_string2();
+	//bit::test_string3();
+	//bit::test_string4();
+	//bit::test_string5();
+	bit::test_string6();
+	//bit_copy::test_string1();
+	return 0;
+}
