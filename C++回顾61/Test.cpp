899494bb9e6d87307bcaa40c92ea3a17b08744da
Codeguer <15946817309@163.com>
2022-02-28 21:45:24 +0800

    2022/2/28

---
+++ C++回顾61/Test.cpp
@@ -0,0 +1,13 @@
+#include"Person.h"
+#include"Person_Student_Teacher.h"
+#include"number.h"
+
+
+int main()
+{
+	bit1::test_person_student_teacher();
+	//bit2::test_person();
+	//bit3::test_number();
+	//bit4::test_bit4();
+	return 0;
+}
