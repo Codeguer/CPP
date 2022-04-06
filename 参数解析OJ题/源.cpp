cc638b65e48f25f4088066336cebe47c8f42a5db
Codeguer <15946817309@163.com>
2022-04-06 22:58:27 +0800

    2022/4/6

---
+++ 参数解析OJ题/源.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main() {
+	string s;
+	getline(cin, s);
+	string tmp("");
+	vector<string>v;
+	bool flag = false;
+	for (int i = 0; i < s.size(); ++i) {
+		if (s[i] == '"')flag = !flag;
+		else if (s[i] == ' '&&flag == false) {
+			v.push_back(tmp);
+			tmp.clear();
+		}
+		else {
+			tmp.push_back(s[i]);
+		}
+	}
+	v.push_back(tmp);
+	cout << v.size() << endl;
+	for (auto &e : v) {
+		cout << e << endl;
+	}
+	return 0;
+}
